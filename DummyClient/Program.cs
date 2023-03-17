92800479194ea2a4031329627c809595c1c13a17
Rina <lhwlina1130@gmail.com>
2023-03-17 21:37:38 +0900

    Create Listener class. Connect With Client async

---
+++ DummyClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace DummyClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName(); // get host name;
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0]; //ip address : 부하분산을 위해 여러개의 ip가 생성될 수 있음. 우선 첫번 째 꺼를 사용함. 
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777); // 7777: port number
+
+            //문지기
+           
+            while (true)
+            {
+                try
+                {
+
+                    Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                    //문지기에게 입장문의
+                    socket.Connect(endPoint);
+
+                    Console.WriteLine("Connect To " + socket.RemoteEndPoint.ToString());
+
+                    //send to server
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                    int sendBytes = socket.Send(sendBuff);
+
+                    //receive from server
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = socket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+
+                    Console.WriteLine("[From Server]" + recvData);
+
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+
+                    Thread.Sleep(1000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception occurred." + e.Message);
+                }
+            }
+
+        }
+
+    }
+
+}
+
+
---
+++ ServerCore/Listener.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+     class Listener
+    {
+        Socket _listenSocket;
+        SocketAsyncEventArgs socketArgs;
+        Action<Socket> connetcionHandler;
+        public void Init(IPEndPoint endPoint, Action<Socket> _handler)
+        {
+            this.connetcionHandler = _handler;
+            socketArgs = new SocketAsyncEventArgs();
+            socketArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //소켓 초기화해줌.
+            _listenSocket.Bind(endPoint);
+
+            //영업시작. 클라에게 요청받을 준비가 완료됨.
+            //최대 10명만 받음. 사람이 몰릴 경우 11번째 부터 fail를 리턴함.
+            _listenSocket.Listen(10);
+                      
+        }
+
+        public void RegisterAccept()
+        {
+            //socketArgs 를 초기화해줌. 재사용
+            socketArgs.AcceptSocket = null;
+
+            //만약 클라에서 요청이 들어올 경우 accept 
+            //비동기방식으로사용해야햄.. 안그럼 대기하다 멈춘다규..
+            //pending == false 라면 대기없이 바로 처리되었다는 의미
+           bool pending =  _listenSocket.AcceptAsync(socketArgs);
+            if (!pending) OnAcceptCompleted(null, socketArgs);
+        }
+        
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if(args.SocketError == SocketError.Success)
+            {
+                Console.WriteLine("Connect!");
+                var clientSocket = args.AcceptSocket;
+                if(clientSocket != null ) connetcionHandler?.Invoke(clientSocket);
+            }
+            else
+            {
+                Console.WriteLine("Socket Listen fail. "+args.SocketError.ToString());
+            }
+
+            //다시 공간이 남았으니 다른 고객을 받을 준비를 함. 
+            RegisterAccept(); 
+        }
+    }
+}
