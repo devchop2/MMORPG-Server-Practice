f18ead84aab8435ef1bdaeb0862c2dc03e1950d5
devchop2 <lhwlina1130@gmail.com>
2023-03-29 22:12:16 +0900

    Serialize/Deserialize String Value

--- DummyClient/ServerSession.cs
+++ DummyClient/ServerSession.cs
@@ -12,18 +12,19 @@ namespace DummyClient
 
         public abstract ArraySegment<byte> Serialize();
         public abstract void Deserialize(ArraySegment<byte> data);
-       
+
     }
 
-    public enum PacketID 
-    {  
+    public enum PacketID
+    {
         PlayerInfoReq = 1,
-        PlayerInfoOk = 2 
+        PlayerInfoOk = 2
     }
 
     class PlayerInfoReq : Packet
     {
         public long playerId;
+        public string name; //가변길
 
         public PlayerInfoReq()
         {
@@ -32,26 +33,69 @@ namespace DummyClient
 
         public override ArraySegment<byte> Serialize()
         {
-            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+            ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+            Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
 
             ushort count = 0;
             bool success = true;
 
             count += sizeof(ushort); //size
 
-            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packetId);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), packetId); ;
             count += sizeof(ushort);
-            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), playerId);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), playerId);
             count += sizeof(long);
 
-            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count - count), count);
-            //패킷사이즈를 계산해서 마지막에 넣어줌. 대신, 첫번째 위치에 넣어줘야함.
+            //send string [size][data]
 
-            return  SendBufferHelper.Close(count);
+            /* version 1
+            ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(name);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), nameLen);
+            count += sizeof(ushort);
+
+            Array.Copy(Encoding.Unicode.GetBytes(name), 0, segment.Array, count, nameLen);
+            count += nameLen;
+            */
+
+
+            //version 2.
+            ushort nameLen = (ushort)Encoding.Unicode.GetBytes(name, 0, name.Length, segment.Array, segment.Offset + count + sizeof(ushort)); //spacing size  Position
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), nameLen);
+
+            count += sizeof(ushort);
+            count += nameLen;
+
+
+            //write size
+            success &= BitConverter.TryWriteBytes(s, count);
+
+            // 클라이언트가 그짓말할거라는거 생각해야 함 
+            //패킷사이즈를 계산해서 마지막에 넣어줌. 대신, 첫번째 위치에 넣어줘야함.
+            if (!success) return null;
+            return SendBufferHelper.Close(count);
         }
+
         public override void Deserialize(ArraySegment<byte> data)
         {
-            throw new NotImplementedException();
+
+            ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(data.Array, data.Offset, data.Count);
+            ushort count = 0;
+            //ushort size = BitConverter.ToUInt16(data.Array, data.Offset);
+            count += sizeof(ushort);
+            //ushort id = BitConverter.ToUInt16(data.Array, data.Offset + count);
+            count += sizeof(ushort);
+
+            //playerId = BitConverter.ToInt64(data.Array, data.Offset + count);
+            playerId = BitConverter.ToInt64(s.Slice(count, s.Length - count));  //좀더 안전한 방법을 사용 . 데이터가 long 사이즈만큼 없을수도있음.
+            count += sizeof(long);
+            Console.WriteLine("[playerId]:" + playerId);
+
+            //read string
+            ushort nameLen = (ushort)BitConverter.ToInt16(s.Slice(count, s.Length - count));
+
+            count += sizeof(ushort);
+            name = Encoding.Unicode.GetString(s.Slice(count, nameLen));
+            Console.WriteLine($"[PlayerName] {name}");
         }
     }
 
@@ -59,15 +103,12 @@ namespace DummyClient
     {
         public override void OnConnected(EndPoint endPoint)
         {
-            Console.WriteLine("On Connected :"+endPoint);
+            Console.WriteLine("On Connected :" + endPoint);
 
-            PlayerInfoReq testPacket = new PlayerInfoReq() {packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
+            PlayerInfoReq testPacket = new PlayerInfoReq() { playerId = 1001, name = "devchop2" };
             var sendBuff = testPacket.Serialize();
 
-            //var sendStr = BitConverter.ToString(sendBuff.Array, sendBuff.Offset, sendBuff.Count); 
-            //Console.WriteLine("Send:"+sendStr);
-
-            Send(sendBuff);
+            if (sendBuff != null) Send(sendBuff);
         }
 
         public override void OnDisconnected(EndPoint endPoint)
--- Server/ClientSession.cs
+++ Server/ClientSession.cs
@@ -4,10 +4,14 @@ using ServerCore;
 
 namespace Server
 {
-    public class Packet
+    public abstract class Packet
     {
         public ushort size;
         public int packetId;
+
+        public abstract ArraySegment<byte> Serialize();
+        public abstract void Deserialize(ArraySegment<byte> data);
+
     }
 
     public enum PacketID
@@ -15,18 +19,75 @@ namespace Server
         PlayerInfoReq = 1,
         PlayerInfoOk = 2
     }
+
     class PlayerInfoReq : Packet
     {
         public long playerId;
+        public string name; //가변길
+
+        public PlayerInfoReq()
+        {
+            packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override ArraySegment<byte> Serialize()
+        {
+            ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+            Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
+
+            ushort count = 0;
+            bool success = true;
+
+            count += sizeof(ushort); //size
+
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), packetId); ;
+            count += sizeof(ushort);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), playerId);
+            count += sizeof(long);
+
+            //send string [size][data]
+            ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(name);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), nameLen);
+            count += sizeof(ushort);
+
+            Array.Copy(Encoding.Unicode.GetBytes(name), 0, segment.Array, count, nameLen);
+            count += nameLen;
+
+            //write size
+            success &= BitConverter.TryWriteBytes(s, count);
+
+            // 클라이언트가 그짓말할거라는거 생각해야 함 
+            //패킷사이즈를 계산해서 마지막에 넣어줌. 대신, 첫번째 위치에 넣어줘야함.
+
+            return SendBufferHelper.Close(count);
+        }
+
+        public override void Deserialize(ArraySegment<byte> data)
+        {
+
+            ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(data.Array, data.Offset, data.Count);
+            ushort count = 0;
+            //ushort size = BitConverter.ToUInt16(data.Array, data.Offset);
+            count += sizeof(ushort);
+            //ushort id = BitConverter.ToUInt16(data.Array, data.Offset + count);
+            count += sizeof(ushort);
+
+            //playerId = BitConverter.ToInt64(data.Array, data.Offset + count);
+            playerId = BitConverter.ToInt64(s.Slice(count, s.Length - count));  //좀더 안전한 방법을 사용 . 데이터가 long 사이즈만큼 없을수도있음.
+            count += sizeof(long);
+            Console.WriteLine("[playerId]:" + playerId);
+
+            //read string
+            ushort nameLen = (ushort)BitConverter.ToInt16(s.Slice(count, s.Length - count));
+
+            count += sizeof(ushort);
+            name = Encoding.Unicode.GetString(s.Slice(count, nameLen));
+            Console.WriteLine($"[PlayerName] {name}");
+        }
     }
 
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int attack;
-    }
 
-    public  class ClientSession : Session
+    public class ClientSession : Session
     {
         public override void OnConnected(EndPoint endPoint)
         {
@@ -46,18 +107,16 @@ namespace Server
             ushort count = 0;
             ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
             count += sizeof(ushort);
-            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            ushort packetId = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
             count += sizeof(ushort);
-               
 
-            switch ((PacketID)id)
+
+            switch ((PacketID)packetId)
             {
                 case PacketID.PlayerInfoReq:
                     {
-                        //todo;
-                        long playerId = BitConverter.ToInt64(buffer.Array, buffer.Offset + count);
-                        count += sizeof(long);
-                        Console.WriteLine("[playerId]:"+playerId);
+                        PlayerInfoReq req = new PlayerInfoReq();
+                        req.Deserialize(buffer);
                         break;
                     }
                 case PacketID.PlayerInfoOk:
@@ -66,7 +125,7 @@ namespace Server
                         break;
                     }
             }
-            
+
             return buffer.Count;
         }
 
